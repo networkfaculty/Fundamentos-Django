9f27a42cf731d326f9ffc7774bd607ab9fbc981f
Juan Paredes <juan.paredes@gmail.com>
2015-10-17 19:47:24 -0430

    Crear un modelo sencillo en Django

--- curso/encuestas/models.py
+++ curso/encuestas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Pregunta(models.Model):
+    texto_pregunta = models.CharField(max_length=200)
+    fe_publicacion = models.DateTimeField('Fecha de Publicación')
+
+
+class Opcion(models.Model):
+    pregunta = models.ForeignKey(Pregunta)
+    texto_opcion = models.CharField(max_length=200)
+    votos = models.IntegerField(default=0)
