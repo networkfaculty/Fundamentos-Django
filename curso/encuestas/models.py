67beb6f69c6cd7dafc29604b05b2b944c01ca0f9
Juan Paredes <juan.paredes@gmail.com>
2015-10-19 13:26:13 -0430

    Agregar funciones en modelo para demostración de uso interactivo

--- curso/encuestas/models.py
+++ curso/encuestas/models.py
@@ -1,13 +1,21 @@
 from django.db import models
-
-# Create your models here.
+from django.utils import timezone
+import datetime
 
 class Pregunta(models.Model):
     texto_pregunta = models.CharField(max_length=200)
     fe_publicacion = models.DateTimeField('Fecha de Publicación')
 
+    def __str__(self):
+        return self.texto_pregunta
+
+    def publicada_recientemente(self, dias=1):
+        return self.fe_publicacion >= timezone.now() - datetime.timedelta(days=dias)
 
 class Opcion(models.Model):
     pregunta = models.ForeignKey(Pregunta)
     texto_opcion = models.CharField(max_length=200)
     votos = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.texto_opcion
