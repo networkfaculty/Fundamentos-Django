18bacc4230ea3247d6895beeaa575c1c89fd6dd1
Juan Paredes <juan.paredes@gmail.com>
2015-10-19 17:09:53 -0430

    Ejemplo de personalización básica de formulario en sitio administrativo

--- curso/encuestas/admin.py
+++ curso/encuestas/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
 
 from .models import Pregunta
+from django.forms import Textarea
+from django.db import models
 
-admin.site.register(Pregunta)
+#class PreguntaAdmin(admin.ModelAdmin):
+    #fields = ['fe_publicacion', 'texto_pregunta']
+
+#class PreguntaAdmin(admin.ModelAdmin):
+    #fieldsets = [
+        #(None, { 'fields': [ 'texto_pregunta' ] }),
+        #('Información adicional', { 'fields': [ 'fe_publicacion' ] } )
+    #]
+
+class PreguntaAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.CharField: {'widget': Textarea(attrs={'style':'width:90%', 'rows':5})},
+    }
+    fieldsets = [
+        (None, { 'fields': [ 'texto_pregunta' ] }),
+        ('Información adicional', {
+            'fields': [ 'fe_publicacion' ],
+            'classes': [ 'collapse' ]
+            }
+        ),
+    ]
+
+admin.site.register(Pregunta, PreguntaAdmin)
