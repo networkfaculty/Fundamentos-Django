b02725e23ead7ce53ffb04053edfd334eb88b4f4
Juan Paredes <juan.paredes@gmail.com>
2015-10-21 13:32:10 -0430

    Ejemplo sencillo de agregar y organizar vistas

--- curso/encuestas/urls.py
+++ curso/encuestas/urls.py
@@ -2,5 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ej: /encuestas/
     url(r'^$', views.index, name='index'),
+    # ej: /encuestas/5
+    url(r'^(?P<id_pregunta>\d+)/$', views.detalle, name='detalle'),
+    # ej: /encuestas/5/resultados
+    url(r'^(?P<id_pregunta>\d+)/resultados$', views.en_construccion, name='resultados'),
+    # ej: /encuestas/5/votar
+    url(r'^(?P<id_pregunta>\d+)/votar$', views.en_construccion, name='votar'),
 ]
--- curso/encuestas/views.py
+++ curso/encuestas/views.py
@@ -1,28 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from django.http import HttpResponse
 
-#def index(request):
-    #return HttpResponse("Este es el listado de encuestas")
-
 from .models import Pregunta
-
-#def index(request):
-    #preguntas_recientes = Pregunta.objects.order_by('-fe_publicacion')[:5]
-    #output = ', '.join([p.texto_pregunta for p in preguntas_recientes])
-    #return HttpResponse(output)
-
-#from django.template import RequestContext, loader
-
-#def index(request):
-    #preguntas_recientes = Pregunta.objects.order_by('-fe_publicacion')[:5]
-    #template = loader.get_template('encuestas/index.html')
-    #context = RequestContext(request, {
-        #'preguntas_recientes': preguntas_recientes,
-    #})
-    #return HttpResponse(template.render(context))
+from django.http import Http404
 
 def index(request):
     preguntas_recientes = Pregunta.objects.order_by('-fe_publicacion')[:5]
     context = { 'preguntas_recientes': preguntas_recientes }
     return render(request, 'encuestas/index.html', context)
+
+#def detalle(request, id_pregunta):
+    #try:
+        #pregunta = Pregunta.objects.get(pk=id_pregunta)
+    #except Pregunta.DoesNotExist:
+        #raise Http404('La pregunta no fue encontrada')
+    #return render(request, 'encuestas/detalle.html', {'pregunta': pregunta})
+
+def detalle(request, id_pregunta):
+    pregunta = get_object_or_404( Pregunta, pk=id_pregunta )
+    return render(request, 'encuestas/detalle.html', {'pregunta': pregunta})
+
+def en_construccion(request, id_pregunta):
+    return HttpResponse('Página '+ request.resolver_match.view_name +
+                        ' en construcción para #'+str(id_pregunta))
